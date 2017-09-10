424e5ade6ba09db3d3de3415e4e24fe7852ba661
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-10 18:46:40 +0530

    code commit

--- src/main/java/com/barath/chatbot/app/rest/ChatBotController.java
+++ src/main/java/com/barath/chatbot/app/rest/ChatBotController.java
@@ -3,10 +3,14 @@ package com.barath.chatbot.app.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
+@CrossOrigin("*")
 public class ChatBotController {
 
     private RestTemplate restTemplate=new RestTemplate();
